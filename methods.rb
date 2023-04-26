c30546a4883d3ee7a4645da7e16aae363e3ae2e2
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-04-25 17:34:48 -0700

    commit tree aa914ee3148bfb01282283ba9e3b6145c5d0c6ea

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
-# Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+greet("Naureeen")
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+  num1 + num2
+end
+
+def halve(num)
+  if num.class != Integer
+    return nil
+  end
+  
+  num / 2
+end
